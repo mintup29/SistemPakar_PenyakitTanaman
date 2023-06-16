a92dff1f6a4401aab69a678a8e8961348fae532c
Fawwaz Ivandra <ivandrafawwaz@gmail.com>
2023-06-16 09:21:53 +0700

    fix faq in homepage

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -61,10 +61,217 @@
     <link rel="apple-touch-icon" href="apple-touch-icon.png">
     <link rel="apple-touch-icon" sizes="72x72" href="apple-touch-icon-72x72.png">
     <link rel="apple-touch-icon" sizes="114x114" href="apple-touch-icon-114x114.png">
-
-
 </head>
 
+<style>
+    .round-btn {display: inline;height: 40px; width: 40px; background:#fff;border-radius: 50%;float: left;margin: 15px 8px;box-shadow: 2px 2px 5px 0px rgb(82, 0, 67);border: 1px solid;/*border: 1px solid #622657;*/}
+    .round-btn a {display: block !important;padding: 7px 12px;font-size: 18px;border-radius: 50%;}
+    .round-btn .icon {padding: 3px;}
+    .round-btn .icon img{height: 24px; width: 32px;margin-top: 6px;}
+    .btn-facebook a {color: #3b5998;padding: 8px 13px;}
+    .btn-linkedin a {color: #007bb6;}
+    .btn-twitter a{color: #1c9deb;}
+    .btn-instagram a{color: #dd3f5c;}
+    .btn-whatsapp a{color: #155E54;}
+    .btn-envelop a{color: #D6403A;font-size: 15px; padding: 9px 12px;}
+    .standard_header .standard_social_links {margin-left: 1rem;}
+
+    /*
+    =========================================================================================
+                                        footer
+    =========================================================================================
+    */
+    
+    .footer-wrap {
+        padding-top: 43px;
+        background-size: cover;
+    }
+
+    .footer-wrap h3 {
+        color: #fff;
+        font-size: 17px;
+        text-transform: uppercase;
+        margin-bottom: 25px;
+    }
+
+    .footer-wrap p {
+        font-size: 13px;
+        line-height: 24px;
+        color: #b3b3b3;
+        margin-top: 15px;
+    }
+
+    .footer-wrap p a {
+        color: #fff;
+        text-decoration: underline;
+        font-style: italic;
+    }
+
+    .footer-wrap p a:hover {
+        text-decoration: none;
+        color: #ff7800;
+    }
+
+    .footer-links li a {
+        font-size: 13px;
+        line-height: 30px;
+        color: #ccc;
+        text-decoration: none;
+    }
+
+
+    .footer-links li:before {
+        content: "\f105";
+        font-family: 'FontAwesome';
+        padding-right: 10px;
+        color: #ccc;
+    }
+
+    .footer-category li a {
+        font-size: 13px;
+        line-height: 30px;
+        color: #ccc;
+        text-decoration: none;
+    }
+
+    .footer-category li:before {
+        content: "\f105";
+        font-family: 'FontAwesome';
+        padding-right: 10px;
+        color: #b3b3b3;
+    }
+
+    .address {
+        
+        color: #b3b3b3;
+        font-size: 14px;
+        position: relative;
+        padding-left: 30px;
+        line-height: 30px;
+    }
+
+    .address:before {
+        content: "\f277";
+        font-family: 'FontAwesome';
+        position: absolute;
+        top: 0;
+        left: 0;
+    }
+
+    .info a {
+    
+        color: #b3b3b3;
+        font-size: 14px;
+        line-height: 30px;
+        font-weight: normal;
+    }
+
+    .fa-phone:before {
+        content: "\f095";
+    }
+
+    .info a {
+
+        color: #b3b3b3;
+        font-size: 14px;
+        line-height: 30px;
+        font-weight: normal;
+    }
+
+    .fa-fax:before {
+        content: "\f1ac";
+    }
+
+    .copyright {
+        border-top: 1px solid #111;
+        font-size: 14px;
+        color: #ccc;
+        padding-top: 15px;
+        text-align: center;
+        padding-bottom: 15px;
+        background: #222;
+    }
+    footer .second_class{
+        border-bottom: 1px solid #444;
+        padding-bottom: 25px;
+    }
+    footer .first_class {
+        padding-bottom: 21px;
+        border-bottom: 1px solid #444;
+    }
+    footer .first_class p, footer .first_class h3{
+        margin: 0 0;
+        
+    }
+    footer{
+        background: #333;
+    }
+
+    footer .newsletter input[type="text"] {
+        width: 100%;
+        background: #fff;
+        color: #333;
+        border: 1px solid #222;
+        padding: 14px 20px;
+        border-radius: 50px;
+        margin-top: 12px;
+    }
+
+    .newsletter .newsletter_submit_btn {
+        background: #fff;
+        position: absolute;
+        right: 30px;
+        border: 0;
+        top: 26px;
+        font-size: 17px;
+        color: #333;
+    }
+
+
+    footer .second_class_bdr{
+        padding-top: 25px;
+        border-top:1px solid #222;
+    }
+
+    footer .btn-facebook a {
+        padding: 6px 14px !important;
+    }
+
+    footer .btn-envelop a {
+        color: #D6403A;
+        font-size: 15px;
+        padding: 12px 12px;
+    }
+
+    footer .round-btn a {
+        padding: 6px 12px;
+    }
+
+    footer .round-btn {
+        box-shadow: 2px 2px 5px 0px #222 !important;}
+
+    footer .round-btn {
+        margin: 15px 4px;}
+    
+    footer dl, ol, ul {
+        padding-left: 5px;
+    }
+    footer li{
+        list-style: none;
+    }
+
+    @media(max-width:768px){
+        .footer-wrap h3 {
+        margin-top: 27px;}
+        
+        footer .round-btn {
+        margin: 15px 4px;}
+    }
+    @media(max-width:320px){
+    .copyright {
+        font-size: 13px;}
+    } 
+</style>
 <body class="royal_preloader">
 
     <!-- <div id="royal_preloader"></div> -->
@@ -134,12 +341,11 @@
                             <br>
                             <style>
                                 .btn-glow:hover {
-                                  box-shadow: 0 0 10px rgba(255, 255, 255, 0.5);
-                                  transition: box-shadow 0.5s;
+                                    box-shadow: 0 0 10px rgba(255, 255, 255, 0.5);
+                                    transition: box-shadow 0.5s;
                                 }
-                              </style>
+                            </style>
                               <div style="margin-left: 40px">
-
                                 <a href="/form-faq"  class="btn btn-dark btn-glow" role="button" style="color: #ffff">
                                   Isi form
                                 </a>
@@ -306,7 +512,7 @@
                             <h2 class="mb-0">
                                 <button class="btn btn-link" type="button" data-toggle="collapse"
                                     data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                                    Apa itu DepresiCheck?
+                                        Apa itu DR.BROKOLI?
                                 </button>
                             </h2>
                         </div>
@@ -314,9 +520,8 @@
                         <div id="collapseOne" class="collapse show" aria-labelledby="headingOne"
                             data-parent="#faqAccordion">
                             <div class="card-body">
-                                DepresiCheck adalah sebuah situs yang membantu mahasiswa akhir mengukur tingkat depresi
-                                mereka dengan mengisi formulir pertanyaan. Kami menyediakan solusi sesuai setelah
-                                mahasiswa mengisi formulir tersebut.
+                                DR.BROKOLI adalah aplikasi sistem pakar yang dirancang untuk membantu petani 
+                                dalam mendiagnosis dan merawat kesehatan tanaman brokoli.
                             </div>
                         </div>
                     </div>
@@ -325,15 +530,16 @@
                             <h2 class="mb-0">
                                 <button class="btn btn-link collapsed" type="button" data-toggle="collapse"
                                     data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                                    Siapa yang bisa mengakses DepresiCheck?
+                                    Bagaimana DR.BROKOLI dapat membantu dalam mendiagnosis kesehatan tanaman brokoli?
                                 </button>
                             </h2>
                         </div>
                         <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo"
                             data-parent="#faqAccordion">
                             <div class="card-body">
-                                DepresiCheck ditujukan untuk mahasiswa akhir, namun siapa saja dapat mengakses situs ini
-                                dan mengisi formulir untuk mengetahui tingkat depresi mereka.
+                                DR.BROKOLI menggunakan kecerdasan buatan dan sistem pakar untuk menganalisis data
+                                yang Anda berikan tentang tanaman brokoli Anda. Berdasarkan informasi tersebut, 
+                                aplikasi akan memberikan diagnosis dan rekomendasi untuk merawat tanaman brokoli secara efektif.
                             </div>
                         </div>
                     </div>
@@ -342,16 +548,16 @@
                             <h2 class="mb-0">
                                 <button class="btn btn-link collapsed" type="button" data-toggle="collapse"
                                     data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                                    Apakah hasil dari DepresiCheck dapat diandalkan?
+                                    Apakah aplikasi DR.BROKOLI dapat memberikan informasi lebih lanjut mengenai perawatan tanaman brokoli?
                                 </button>
                             </h2>
                         </div>
                         <div id="collapseThree" class="collapse" aria-labelledby="headingThree"
                             data-parent="#faqAccordion">
                             <div class="card-body">
-                                Hasil dari DepresiCheck adalah sebuah estimasi dari tingkat depresi seseorang, dan tidak
-                                bisa dianggap sebagai diagnosis yang pasti. Kami sangat menyarankan agar seseorang yang
-                                merasa memiliki tingkat depresi yang tinggi untuk segera meminta bantuan profesional.
+                                Ya, selain memberikan diagnosis dan rekomendasi, aplikasi DR.BROKOLI juga menyediakan informasi 
+                                dan sumber daya berguna mengenai perawatan tanaman brokoli. Anda dapat menemukan panduan, tips, 
+                                dan praktik terbaik untuk merawat tanaman brokoli dengan baik.
                             </div>
                         </div>
                     </div>
@@ -360,16 +566,16 @@
                             <h2 class="mb-0">
                                 <button class="btn btn-link collapsed" type="button" data-toggle="collapse"
                                     data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
-                                    Bagaimana cara mengakses solusi yang ditawarkan DepresiCheck?
+                                    Bagaimana jika saya memiliki pertanyaan atau masalah yang tidak tercakup di DR.BROKOLI?
                                 </button>
                             </h2>
                         </div>
                         <div id="collapseFour" class="collapse" aria-labelledby="headingFour"
                             data-parent="#faqAccordion">
                             <div class="card-body">
-                                Setelah mengisi formulir, mahasiswa akan menerima rekomendasi solusi sesuai dengan
-                                tingkat depresi yang terdeteksi. Kami juga menyediakan tautan ke sumber informasi dan
-                                bantuan profesional yang dapat membantu mahasiswa mengatasi depresi mereka.
+                                Jika Anda memiliki pertanyaan atau masalah yang tidak tercakup di DR.BROKOLI, silakan hubungi tim kami 
+                                melalui halaman "Kontak Kami" di website kami. Kami akan dengan senang hati membantu 
+                                Anda dengan pertanyaan atau masalah yang Anda hadapi.
                             </div>
                         </div>
                     </div>
@@ -378,37 +584,121 @@
             </div>
         </section>
 
-        <div class="section footer padding-top-big background-image-cover"
-            style="background-image: url(landing/img/footer.jpg);">
-            <div class="container">
+        <!--footer start-->
+        <footer>
+            <div class="footer-wrap">
+            <div class="container first_class">
                 <div class="row">
-                    <div class="col-md-12">
-                        <img src="landing/img/logo-light.png" alt="logo image" />
-                    </div>
-                    <div class="col-md-12 my-4">
-                        <ul class="footer-social">
-                            <li>
-                                <a href="#">Tw</a>
-                            </li>
-                            <li>
-                                <a href="#">Fb</a>
-                            </li>
-                            <li>
-                                <a href="#">G+</a>
-                            </li>
-                            <li>
-                                <a href="#">Vm</a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div class="col-md-12 mt-5">
-                        <div class="footer-line"></div>
-                    </div>
-                    <div class="col-md-12 rights my-3">
-                    </div>
+                <div class="col-md-4 col-sm-6">
+                    <h3>BE THE FIRST TO KNOW</h3>
+                    <p>Get all the latest information on  Triedge Services, Events, Jobs
+                    and Fairs. Sign up for our newsletter today.</p>
+                </div>
+                <div class="col-md-4 col-sm-6">
+                <form class="newsletter">
+                    <input type="text" placeholder="Email Address"> 
+                        <button class="newsletter_submit_btn" type="submit"><i class="fa fa-paper-plane"></i></button>  
+                </form>
+                
+                </div>
+                <div class="col-md-4 col-sm-6">
+                <div class="col-md-12">
+                    <div class="standard_social_links">
+                <div>
+                    <li class="round-btn btn-facebook"><a href="#"><i class="fab fa-facebook-f"></i></a>
+        
+                    </li>
+                    <li class="round-btn btn-linkedin"><a href="#"><i class="fab fa-linkedin-in" aria-hidden="true"></i></a>
+        
+                    </li>
+                    <li class="round-btn btn-twitter"><a href="#"><i class="fab fa-twitter" aria-hidden="true"></i></a>
+        
+                    </li>
+                    <li class="round-btn btn-instagram"><a href="#"><i class="fab fa-instagram" aria-hidden="true"></i></a>
+        
+                    </li>
+                    <li class="round-btn btn-whatsapp"><a href="#"><i class="fab fa-whatsapp" aria-hidden="true"></i></a>
+        
+                    </li>
+                    <li class="round-btn btn-envelop"><a href="#"><i class="fa fa-envelope" aria-hidden="true"></i></a>
+        
+                    </li>
+                </div>
+                </div>  
+                </div>
+                    <div class="clearfix"></div>
+                <div class="col-md-12"><h3 style="text-align: right;">Stay Connected</h3></div>
+                </div>
                 </div>
             </div>
-        </div>
+            <div class="second_class">
+                <div class="container second_class_bdr">
+                <div class="row">
+                <div class="col-md-4 col-sm-6">
+                    <p>Your one-stop career platform to find Jobs, Internships, Professional Trainings, Projects, and Volunteering Opportunities.</p>
+                </div>
+                <div class="col-md-2 col-sm-6">
+                    <h3>Quick  LInks</h3>
+                    <ul class="footer-links">
+                    <li><a href="#">Home</a>
+                    </li>
+                    <li><a href="#">About us</a>
+                    </li>
+                    <li><a href="#">Triedge Partners</a>
+                    </li>
+                    <li><a href="#">Contact Us</a>
+                    </li>
+                    <li><a href="#" target="_blank">Terms &amp; Conditions</a>
+                    </li>
+                    <li><a href="#" target="_blank">Privacy Policy</a>
+                    </li>
+                    <li><a href="#">Sitemap</a>
+                    </li>
+                    </ul>
+                </div>
+                <div class="col-md-3 col-sm-6">
+                    <h3>OUR SERVICES</h3>
+                    <ul class="footer-category">
+                    <li><a href="#">Fresher Jobs</a>
+                    </li>
+                    <li><a href="#">InternEdge - Internships &amp; Projects </a>
+                    </li>
+                    <li><a href="#">Resume Edge - Resume Writing Services</a>
+                    </li>
+                    <li><a href="#">Readers Galleria - Curated Library</a>
+                    </li>
+                    <li><a href="#">Trideus - Campus Ambassadors</a>
+                    </li>
+                    </ul>
+                    <div class="clearfix"></div>
+                </div>
+                <div class="col-md-3 col-sm-6">
+                    <h3>triedge Events</h3>
+                    <ul class="footer-links">
+                    <li><a href="#">Triedge Events</a>
+                    </li>
+        
+                    <li><a href="#">Jobs &AMP; Internship Fair 2019</a>
+                    </li>
+                    </ul>
+                </div>
+                </div>
+                
+            </div>
+            </div>
+            
+            <div class="row">
+                
+                <div class="container-fluid">
+                <div class="copyright"> Copyright 2019 | All Rights Reserved by TRIEDGE Solutions Pvt. Ltd.</div>
+                </div>
+                
+            </div>
+            </div>
+            
+            </footer>
+        
+        <!--footer end-->
     </main>
 
 
@@ -420,6 +710,7 @@
     <script src="landing/js/bootstrap.min.js"></script>
     <script src="landing/js/plugins.js"></script>
     <script src="landing/js/custom.js"></script>
+    <script src="https://kit.fontawesome.com/fddf5c0916.js" crossorigin="anonymous"></script>
     <script>
         // Type text
 
@@ -435,6 +726,11 @@
             cursorChar: "_",
             attr: null
         });
+
+    
+
+    <!-- Font Awesome 5 links-->
+
     </script>
     <!-- End Document
 ================================================== -->
